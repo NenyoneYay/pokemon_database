fccee5afe7ec781173cea002e5472b749163bddd
Umbrial <Umbreon.Death@Gmail.com>
2026-04-21 17:39:06 -0600

    statistics more than meets the py

--- dicemath.py
+++ dicemath.py
@@ -1,8 +1,18 @@
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 #comment land
 i=0
 averagerolls = []
+
+statsum = []
+strn = 0
+dex = 0
+con = 0
+inte = 0
+wis = 0
+cha = 0
 
 
 def rolling():
@@ -31,6 +41,7 @@ def statTotal():
 
 while(i<10000):
    averagerolls.append(statTotal())
+   statsum.append(statTotal()+statTotal()+statTotal()+statTotal()+statTotal()+statTotal())
    i += 1
 
 averagerolls.sort()
@@ -52,7 +63,7 @@ def basicstats():
     print(f"ceilinged average: {listmean}")
 
     #print(f"number of 16's {averagerolls.count(16)}")
-    specifi = 27
+    specifi = 16
     chanceOf = averagerolls.count(specifi)
     chanceOf = chanceOf/10000
     chanceOf = chanceOf*100
@@ -61,3 +72,17 @@ def basicstats():
     return
 
 basicstats()
+
+xAxis = np.array(["6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"])
+yAxis = np.array([averagerolls.count(6),averagerolls.count(7),averagerolls.count(8),averagerolls.count(9),averagerolls.count(10),averagerolls.count(11),averagerolls.count(12),averagerolls.count(13),averagerolls.count(14),averagerolls.count(15),averagerolls.count(16),averagerolls.count(17),averagerolls.count(18),averagerolls.count(19),averagerolls.count(20),averagerolls.count(21),averagerolls.count(22),averagerolls.count(23),averagerolls.count(24),averagerolls.count(25),averagerolls.count(26),averagerolls.count(27),averagerolls.count(28),averagerolls.count(29),averagerolls.count(30)])
+
+standardDev = np.std(averagerolls)
+print(standardDev)
+
+statdev = np.std(statsum)
+statmean = np.mean(statsum)
+print(f"standard deviation: {statdev}")
+print(f"stats mean: {statmean}")
+
+#plt.bar(xAxis,yAxis)
+#plt.show()
